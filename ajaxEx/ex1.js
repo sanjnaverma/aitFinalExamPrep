d59865a5e85978e0cb20337889c556bbe04d5a33
sanjnaverma <sanjnaverma@nyu.edu>
2016-05-12 00:40:44 -0400

    ajax exercises

---
+++ ajaxEx/ex1.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", main);
+
+function main(){
+
+	var thisDiv = document.getElementById("content");
+	//console.log(thisDiv);
+	//var getA = document.querySelector('a');
+	//console.log(getA);
+	//var newHTML = "<p>stuff3 <a href=\"example.com\">with a link</a></p>";
+	//console.log(getA.innerHTML);
+	//getA.innerHTML = newHTML;
+	//console.log("****");
+
+	//console.log(getA);
+	console.log("he");
+	var element = document.getElementById("cake");
+	var cur = element.href;
+	console.log(cur);
+	
+
+	console.log("88888888888");
+
+	var ele2 = element.class;
+	console.log(ele2);
+}
---
+++ ajaxEx/readMe.md
@@ -0,0 +1,18 @@
+First example: 
+
+Assuming the following markup within a body tag, how would you change the href of the last link so that it takes you to an image search for cookies (using JavaScript)?
+
+<div id="announcement">
+  announcement
+</div>
+<div id="content">
+  content
+  <p class="body-text"> stuff 1 </p>
+  <p class="body-text"> stuff 2 </p>
+  <p> stuff 3 <a href="">with a link </a></p>
+  <p> Um Cookies <a id="cookies" href="">🍪 🍪 🍪 🍪 🍪</a></p>
+</div>
+
+See answer in ex1.js
+
+
